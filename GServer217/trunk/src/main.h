a1262a0c3da14ec60cbb9ba9740058d2558aef06
loneboco <loneboco@users.noreply.github.com>
2008-02-07 04:26:26 +0000

    * Set the build number to 55.

--- GServer217/trunk/src/main.h
+++ GServer217/trunk/src/main.h
@@ -12,7 +12,7 @@
 #include "CWordFilter.h"
 #include <time.h>
 
-#define GSERVER_BUILD 54
+#define GSERVER_BUILD 55
 //#define STORE_SQL
 
 extern bool apSystem, bushesDrop, cheatwindowsban, dontaddserverflags, dontchangekills, dropItemsDead, globalGuilds, lsConnected, noExplosions, serverRunning, setbodyallowed, setheadallowed, setswordallowed, setshieldallowed, showConsolePackets, showQuery, staffOnly, vasesDrop, warptoforall, defaultweapons;
